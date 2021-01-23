f92cb080561445a04ca653d21a2be7a6544f9ce1
veerr <kakarlaveerraju2010@gmail.com>
2021-01-24 00:05:02 +0530

    Veerraju: Implemented Spring3 project

---
+++ src/main/java/com/vkakarla/spring3/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.vkakarla.spring3;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+private String name;
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public void printHello() {
+    System.out.println("Spring 3 : Hello ! " + name);
+}
+}
